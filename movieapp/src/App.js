5fe1925acbad5432acaefe24949c2f0a4e5f6715
Ashu <ashu@gmail.com>
2022-04-12 14:51:57 +0530

    Added my project

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,34 +1,52 @@
-import './App.css';
-import NewExpense from './components/newExpense/NewExpense';
+import React, { useState } from 'react';
+import NewExpense  from './components/newExpense/NewExpense'
+
 import Expenses from './components/Expenses/Expenses';
 
-function App() {
-  const expense = [
-    {
-      id: 'e1',
-      title: 'Car Rent',
-      amount: 120,
-      date: new Date(2022, 3, 14)
-    },
-    {
-      id: 'e2',
-      title: 'room Rent',
-      amount: 12000,
-      date: new Date(2022, 3, 14)
-    },
-    {
-      id: 'e3',
-      title: 'Demart',
-      amount: 1200,
-      date: new Date(2022, 3, 14)
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <NewExpense/>
-      <Expenses items = {expense}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- movieapp/src/components/Expenses/Expenses.js
+++ movieapp/src/components/Expenses/Expenses.js
@@ -1,26 +1,34 @@
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from 'react';
+
 import Card from './Card';
-import './Expenses.css'
-function Expenses({items}) {
-  
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
-    <Card className="expenses">
-    <ExpenseItem
-      title={items[0].title}
-      amount={items[0].amount}
-      date={items[0].date}>
-    </ExpenseItem>
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}>
-      </ExpenseItem>
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}>
-      </ExpenseItem>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
-  )
-}
+    </div>
+  );
+};
+
 export default Expenses;
